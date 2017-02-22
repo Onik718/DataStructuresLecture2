f6b3b94813a256ccae49a7acef000fa327cba21b
jessehartloff <hartloff@buffalo.edu>
2017-02-21 19:23:26 -0500

    initial commit

---
+++ src/DataStructuresLecture2.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class DataStructuresLecture2 {
+
+
+    // 2 points
+    static boolean Q1(HashMap<Character, Integer> inputMap) {
+
+        // TODO: Return true if inputMap has more than 2 key-value pairs, false otherwise
+
+
+        return true;
+    }
+
+
+    // 3 points
+    static HashMap<String, Integer> Q2(String key, int value) {
+
+        // TODO: Return a new HashMap containing the input value at the input key as its only key-value pair
+
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
